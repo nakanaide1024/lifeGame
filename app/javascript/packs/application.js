348cc535a5391558d118c92c5bd852a4cc5a9ee9
nakanaide1024 <71699010+nakanaide1024@users.noreply.github.com>
2020-12-14 14:12:39 +0900

    参考 JSの追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,11 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+require("../app")
+require("../board")
+require("../cell")
+require("../canvas")
+require("../lifeWorld")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
